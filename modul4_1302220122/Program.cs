b76f9e932dae538719eeb298188a1d31a308d08f
salmannhr <138693411+salmannhr@users.noreply.github.com>
2024-03-16 09:07:38 +0700

    menambahkan class KodeBuah

--- modul4_1302220122/Program.cs
+++ modul4_1302220122/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class kodebuah
+{
+    public enum namabuah
+    { apel, aprikot, alpukat, pisang, paprika, blackberry, ceri, kelapa, jagung}
+
+    public static string getkodebuah(namabuah namabuah)
+    {
+        String[] kodebuah = { "AA0", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+            return kodebuah[(int)namabuah];
+    }
+    static void Main(string[] args)
+    {
+        kodebuah namabuah = new kodebuah();
+        Console.WriteLine("Nama buah = " + namabuah);
+        string namabuah = Console.ReadLine();
+        String kodebuah = kodebuah.getkodebuah();
+    }
+}
